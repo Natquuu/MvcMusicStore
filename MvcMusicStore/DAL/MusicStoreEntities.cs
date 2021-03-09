8e5d5e78e9d299a05029fb08ae93da073aa7f0b1
Natquuu <nataliahebda7@gmail.com>
2021-03-09 20:35:44 +0100

    Dodaj pliki projektów.

---
+++ MvcMusicStore/DAL/MusicStoreEntities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcMusicStore.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MvcMusicStore.DAL
+{
+    public class MusicStoreEntities : DbContext
+    {
+        public MusicStoreEntities() : base("MusicStoreEntities")
+        {
+        }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
